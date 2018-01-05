707f841206cd36af669fa8f5596caa63892a112b
Shivang Srivastava <satyamsri8@users.noreply.github.com>
2018-01-05 09:22:07 +0530

    Project Finished
    Accuracy 97% in 200 Epocs and Adding data_epoc200.mat containing all the data for future reference

--- main.m
+++ main.m
@@ -5,10 +5,12 @@ clc
 initilization
 
 % Calling Training.m Script
-for epoc = 1:20
+for epoc = 1:200
     alpha = 1/(epoc^0.5);
     %alpha = 0.5;
     training
 end
 
 Accuracy
+
+save 'C:\Users\acer\Desktop\Digit Recognition\data_epoc200.mat'
